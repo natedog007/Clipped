3bd82cbd4ab8a2ff12cd3d8c7fa5f7d36b6e955f
Nathan Boyden <nrb121903@gmail.com>
2025-09-01 20:35:00 -0400

    asd

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import { ChevronFirst } from "lucide-react";
+
+function Sidebar() {
+    return (
+        <aside className="h-screen">
+            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+                <div className="p-4 flex justify-between items-center">
+                    <img 
+                        src="https://logoipsum.com/artwork/397" 
+                        alt="Logo"
+                        className="w-32"
+                    />
+                    <button className="p-1.5 rounded-lg hover:bg-gray-100">
+                        <ChevronFirst />
+                    </button>
+                </div>
+            </nav>
+        </aside>
+    );
+}
+
+export default Sidebar;
